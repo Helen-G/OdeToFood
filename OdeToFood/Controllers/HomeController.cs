1510d5f43b4a68a7c630a41393bc601e341daba5
heleng <len4ikpost@gmail.com>
2016-03-01 14:58:24 -0800

    initial commit

---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            var model = new RestaurantReview()
+            {
+                Name = "Tersiguel",
+                Rating = 9
+            };
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
